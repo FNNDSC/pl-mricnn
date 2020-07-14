74e3b67aeda73eb2927d498473049bd3db6a5361
Sandip Samal <32147904+Sandip117@users.noreply.github.com>
2020-07-14 00:26:56 -0400

    Update mricnn.py

--- mricnn/mricnn.py
+++ mricnn/mricnn.py
@@ -267,8 +267,8 @@ class Mricnn(ChrisApp):
 
 
         imgs_train, imgs_mask_train = load_train_data(options)
-        imgs_mask_train = imgs_mask_train.astype('float32')
-        imgs_train = imgs_train.astype('float32')
+        imgs_mask_train = imgs_mask_train.astype('float64')
+        imgs_train = imgs_train.astype('float64')
         imgs_mask_train /= 255.  # scale masks to [0, 1]
         imgs_train /= 255.  # scale masks to [0, 1]
 
